157936283cbbf39903a992916dcb9e2db9b0f564
Fincra Dev <chukwuemerie@fincra.com>
2026-01-10 23:13:48 +0100

    add new folders about and heritage to the next js file structure

---
+++ src/app/(pages)/about/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/landingpage/contents/footer";
+import Header from "@/components/landingpage/navigations /header";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Header />
+      About page
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/(pages)/heritage/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/landingpage/contents/footer";
+import Header from "@/components/landingpage/navigations /header";
+import React from "react";
+
+const HeritagePage = () => {
+  return (
+    <div>
+      <Header/>
+      heritage Page
+      <Footer />
+    </div>
+  );
+};
+
+export default HeritagePage;
