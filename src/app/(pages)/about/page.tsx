f6a50dcc1691589c442080737c438095325448e7
Fincra Dev <chukwuemerie@fincra.com>
2026-01-17 03:23:30 +0100

    about us

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -9,7 +9,7 @@ const AboutPage = () => {
       <Header />
       <main className="w-full overflow-x-hidden">
         <div className="mobile-bg relative bg-cover w-full flex items-center bg-no-repeat opacity-  h-100vh lg:h-[70vh] lg:pt-0 overflow-x-hidden ">
-          <div className="grid lg:grid-cols-2 gap-1 items-start w-full lg:w-10/12 mx-auto p-8 mt-25 ">
+          <div className="grid 2xl:grid-cols-2 gap-1 items-start w-full 2xl:w-10/12 mx-auto p-4 mt-16  ">
             <motion.div
               initial={{ opacity: 0, x: 30 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -17,11 +17,11 @@ const AboutPage = () => {
               transition={{ duration: 0.6, delay: 0.2 }}
               className=" w-full lg:h-72 "
             >
-              <div className="flex flex-col items-center h-full  w-full p-6">
-                <h2 className="font-serif text-2xl lg:text-7xl text-center font-bold mb-8 text-[#0F3D2E]">
+              <div className="flex flex-col items-center h-full space-y-4 w-full p-6 mb-">
+                <h2 className="font-serif text-2xl md:text-5xl lg:text-7xl text-center font-bold  text-[#0F3D2E]">
                   OMO’BA ADEDOTUN A. OLATEJU-JAGUN
                 </h2>
-                <p className="font-sans text-base lg:text-3xl leading-relaxed lg:mb-8 text-center">
+                <p className="font-sans text-base md:text-2xl lg:text-3xl leading-relaxed md:mb-0 mb-4 text-center">
                   Omo’ba Afidipotemole ti Ijebu-Ode
                 </p>
               </div>
@@ -34,7 +34,7 @@ const AboutPage = () => {
             >
               <div className="p-2 flex items-center justify-center h-72 ">
                 <div className="rounded flex items-center justify-center">
-                  <p className="font-sans text-lg lg:text-2xl leading-relaxed mb-8">
+                  <p className="font-sans text-lg md:text-2xl lg:text-2xl leading-relaxed mb-8">
                     Prince Otunba Adedotun Adebola Babatunde Jagun is a
                     community-focused entrepreneur and cultural leader deeply
                     rooted in Ijebu Ode. His work reflects a lifelong commitment
@@ -47,20 +47,20 @@ const AboutPage = () => {
             </motion.div>
           </div>
         </div>
-        <div className="py-16 lg:py-14 w-full bg-white overflow-x-hidden ">
-          <div className="grid lg:grid-cols-2 gap-1 items-start w-full lg:w-10/12 mx-auto p-4 lg:p-8 ">
+        <div className="py-4 2xl:py-14 w-full bg-white overflow-x-hidden ">
+          <div className="grid 2xl:grid-cols-2 gap-1 items-start w-full 2xl:w-10/12 mx-auto p-4 lg:p-8  ">
             <motion.div
               initial={{ opacity: 0, x: 30 }}
               whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: false, amount: 0.3 }}
+              viewport={{ once: true, amount: 0.3 }}
               transition={{ duration: 0.6, delay: 0.2 }}
               className=" w-full h-full"
             >
-              <div className="flex flex-col justify-center items-center h-full w-full md:p-6">
-                <h2 className="font-serif text-2xl text-center lg:text-6xl font-bold text-[#0F3D2E] mb-8  ">
+              <div className="flex flex-col justify-center items-center h-full w-full 2xl:p-6">
+                <h2 className="font-serif text-2xl text-center md:text-4xl 2xl:text-6xl font-bold text-[#0F3D2E] mb-8  ">
                   A Legacy in Motion
                 </h2>
-                <p className="font-sans text-lg lg:text-3xl leading-relaxed mb-8">
+                <p className="font-sans text-lg md:text-2xl 2xl:text-3xl leading-relaxed mb-8">
                   Born in the United States and raised between Nigeria and the
                   U.S., Prince Adedotun embodies a global perspective grounded
                   in tradition. A proud member of the Fidipote Ruling House of
@@ -72,11 +72,11 @@ const AboutPage = () => {
             <motion.div
               initial={{ opacity: 0, x: -30 }}
               whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: false, amount: 0.3 }}
+              viewport={{ once: true, amount: 0.3 }}
               transition={{ duration: 0.6 }}
             >
               <div className=" aspect-auto rounded flex items-center justify-center">
-                <div className="h-120 w-full lg:h-160 lg:w-135.25 lg:p-0 p-2">
+                <div className="h-120 w-full md:h-270 2xl:h-160 2xl:w-135.25 lg:p-0 p-2">
                   <Image
                     src="/images/about2.jpg"
                     alt="Prince seated on throne"
@@ -95,13 +95,13 @@ const AboutPage = () => {
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: false, amount: 0.3 }}
           transition={{ duration: 0.6, delay: 0.2 }}
-          className="w-full justify-center flex "
+          className="w-full justify-center flex md:p-4 2xl:p-0 "
         >
-          <div className="flex flex-col justify-center items-center h-fit w-full lg:w-[85%] py-6 md:px-16 px-4">
-            <h2 className="font-serif text-center text-2xl lg:text-6xl font-bold text-[#0F3D2E] mb-6 ">
+          <div className="flex flex-col justify-center items-center h-fit w-full 2xl:w-[85%] py-6 2xl:px-16 px-4">
+            <h2 className="font-serif text-center text-2xl md:text-4xl 2xl:text-6xl font-bold text-[#0F3D2E] mb-6 ">
               Early Life & Foundation
             </h2>
-            <p className="font-sans text-lg lg:text-2xl leading-relaxed">
+            <p className="font-sans text-lg md:text-2xl 2xl:text-2xl leading-relaxed">
               Prince Adedotun’s formative years were shaped by movement across
               cultures and communities. Educated at Saint Finbar’s College,
               Akoka, he developed discipline, leadership, and a strong sense of
@@ -117,16 +117,16 @@ const AboutPage = () => {
             </p>
           </div>
         </motion.div>
-        <div className="mx-auto w-full lg:max-w-[85%] py-0 lg:py-6 lg:px-16 px-4 ">
-          <div className="flex lg:flex-row flex-col w-full justify-center items-center gap-8 lg:gap-4  mb-6">
+        <div className="mx-auto w-full 2xl:max-w-[85%] py-0 2xl:py-6 2xl:px-16 px-4  ">
+          <div className="flex 2xl:flex-row flex-col w-full justify-center items-center gap-8 2xl:gap-4  mb-6">
             <motion.div
               initial={{ opacity: 0, x: -30 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: false, amount: 0.3 }}
               transition={{ duration: 0.6 }}
-              className="w-full lg:w-1/2 h-160 flex flex-col justify-center items-start text-center"
+              className="w-full 2xl:w-1/2 h-140 md:h-270 flex flex-col justify-center items-start text-center"
             >
-              <div className="h-120 w-full lg:h-140 lg:w-135.25 ">
+              <div className="h-120 w-full md:h-260 md:w-full 2xl:h-140 2xl:w-135.25 ">
                 <Image
                   src="/images/about3.png"
                   alt="Prince seated on throne"
@@ -135,7 +135,7 @@ const AboutPage = () => {
                   height={100}
                   priority
                 />
-                <h2 className="font-sans text-lg md:text-xl font-normal mt-4">
+                <h2 className="font-sans text-lg md:text-2xl font-normal mt-4">
                   A sculptured of Awùjalès S. K. Adetona{" "}
                 </h2>
               </div>
@@ -146,13 +146,13 @@ const AboutPage = () => {
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: false, amount: 0.3 }}
               transition={{ duration: 0.6, delay: 0.2 }}
-              className=" w-full lg:w-1/2 lg:h-160 "
+              className=" w-full 2xl:w-1/2 2xl:h-160 md:mt-14 "
             >
               <div className="flex flex-col justify-center items-center h-full w-full">
-                <h2 className="font-serif text-center text-2xl md:text-5xl font-bold mb-6 text-[#0F3D2E]">
+                <h2 className="font-serif text-center text-2xl md:text-4xl font-bold mb-6 text-[#0F3D2E]">
                   Roots of Royal Heritage
                 </h2>
-                <p className="font-sans text-lg lg:text-2xl leading-relaxed lg:mb-0 mb-8">
+                <p className="font-sans text-xl 2xl:text-2xl leading-relaxed lg:mb-0 mb-8">
                   Prince Adedotun is a direct descendant of Awujale Ademuyewo
                   Fidipote, who reigned from 1850 to 1886, and the
                   great-grandson of Princess Adefowope Ademuyewo and ‘Dada’
@@ -164,17 +164,17 @@ const AboutPage = () => {
           </div>
         </div>
 
-        <div className="py-16 lg:py-14 w-full bg-[#0F3D2E] p-4 lg:p-0">
-          <div className="grid lg:grid-cols-2 gap-1 items-start justify-between bg-white  w-full lg:w-10/12 mx-auto p-8 shadow-lg lg:h-160">
+        <div className="py-16 2xl:py-14 w-full bg-[#0F3D2E] p-4 2xl:p-0">
+          <div className="grid lg:grid-cols-2 gap-1 items-start justify-between bg-white  w-full 2xl:w-10/12 mx-auto md:p-4 p-2 2xl:p-8 shadow-lg lg:h-160">
             <motion.div
               initial={{ opacity: 0, x: 30 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: false, amount: 0.3 }}
               transition={{ duration: 0.6, delay: 0.2 }}
-              className=" w-full h-full"
+              className=" w-full h-full "
             >
               <div className="flex flex-col justify-center items-center h-full w-full p-0 lg:p-6">
-                <h2 className="text-2xl text-center font-serif lg:text-5xl font-bold text-[#0F3D2E] mb-8 ">
+                <h2 className="text-2xl text-center font-serif  md:text-4xl  2xl:text-5xl font-bold text-[#0F3D2E] mb-8 ">
                   Lineage of Leadership
                 </h2>
                 <p className="font-sans text-lg lg:text-2xl text-left leading-relaxed mb-8">
@@ -196,7 +196,7 @@ const AboutPage = () => {
               <div className=" aspect-auto rounded w-full flex items-center justify-end h-full ">
                 <div className="h-full w-full flex items-center justify-end">
                   <Image
-                    src="/images/about-oba.jpg"
+                    src="/images/grandpa.jpg"
                     alt="Prince seated on throne"
                     className="w-full h-full object-cover rounded-2xl lg:h-148 mb-2 lg:w-135.25"
                     width={800}
@@ -208,13 +208,13 @@ const AboutPage = () => {
             </motion.div>
           </div>
         </div>
-        <div className=" mx-auto py-6 lg:py-14 bg-red00  w-full  lg:w-10/12">
-          <div className="grid lg:grid-cols-2 gap-1 items-start w-full justify-between lg:h-160">
+        <div className=" mx-auto p-4 2xl:py-14  w-full  2xl:w-10/12">
+          <div className="grid lg:grid-cols-2 gap-1 items-start w-full justify-center 2xl:h-160 ">
             <div className="p-4 lg:hidden">
-              <h2 className="text-2xl text-center font-serif lg:text-4xl font-bold text-[#0F3D2E] mb-8 ">
+              <h2 className="text-2xl text-center font-serif md:text-4xl 2xl:text-4xl font-bold text-[#0F3D2E] mb-8 ">
                 Values & Social Commitment
               </h2>
-              <p className="font-sans text-lg lg:text-2xl leading-relaxed ">
+              <p className="font-sans text-lg 2xl:text-2xl leading-relaxed ">
                 Guided by a humanitarian philosophy, Prince Adedotun believes
                 communities thrive through access to education, skills, food,
                 and opportunity. His work supports youth development,
@@ -231,7 +231,7 @@ const AboutPage = () => {
               className="w-full lg:h-160 "
             >
               <div className="aspect-auto rounded flex items-start order-2 p-4 lg:p-0">
-                <div className="h-120 w-full lg:h-150 lg:w-125.25">
+                <div className="h-120 w-full md:h-250 lg:h-150 lg:w-125.25">
                   <Image
                     src="/images/frame4.jpg"
                     alt="Prince seated on throne"
@@ -253,10 +253,10 @@ const AboutPage = () => {
             >
               <div className="flex flex-col justify-center items-center h-full gap-3 w-full p-4 lg:p-6  ">
                 <div className="hidden lg:flex lg:flex-col ">
-                  <h2 className="text-2xl  font-serif lg:text-4xl font-bold text-[#0F3D2E] mb-8 text-center">
+                  <h2 className="text-2xl  font-serif  md:text-4xl  2xl:text-4xl font-bold text-[#0F3D2E] mb-8 text-center">
                     Values & Social Commitment
                   </h2>
-                  <p className="font-sans text-lg lg:text-2xl leading-relaxed mb-8">
+                  <p className="font-sans text-lg 2xl:text-2xl leading-relaxed mb-8">
                     Guided by a humanitarian philosophy, Prince Adedotun
                     believes communities thrive through access to education,
                     skills, food, and opportunity. His work supports youth
@@ -265,10 +265,10 @@ const AboutPage = () => {
                   </p>
                 </div>
 
-                <h2 className="text-3xl text-center font-serif lg:text-4xl font-bold text-[#0F3D2E] mb-8  ">
+                <h2 className="text-3xl text-center m font-serif md:text-4xl  2xl:text-4xlfont-bold text-[#0F3D2E] ">
                   Continuing The Legacy
                 </h2>
-                <p className="font-sans text-lg lg:text-2xl leading-relaxed order-2 lg:order-1">
+                <p className="font-sans text-lg 2xl:text-2xl leading-relaxed order-2 lg:order-1">
                   Prince Otunba Adedotun Adebola Babatunde Jagun represents a
                   balance of heritage and progress. His journey reflects respect
                   for tradition, guided by a clear vision for sustainable
--- src/app/(pages)/heritage/page.tsx
+++ src/app/(pages)/heritage/page.tsx
@@ -171,8 +171,8 @@ const HeritagePage = () => {
             </div>
           </div>
         </div>
-        <div className="2xl:w-1/3 md:w-full h-full flex items-center justify-center mb-8 2xl:mb-0">
-          <div className="w-full md:h-full h-11/12   lg:p-0 flex justify-center items-center ">
+        <div className="2xl:w-1/3 md:w-full h-full flex items-center justify-center mb-8 2xl:mb-0 bg-amber-400">
+          <div className="w-full md:h-full h-11/12  lg:p-0 flex justify-center items-center ">
             <Image
               src="/images/heritage2.png"
               alt="Prince seated on throne"
